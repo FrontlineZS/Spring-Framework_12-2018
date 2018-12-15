e094e07513cb24bfcbafe8d33ee63c4d863236cd
radoslaw.kochaniuk <radoslaw.kochaniuk@assecods.pl>
2018-12-15 23:19:53 +0100

    setup

---
+++ frontline/src/main/java/zs/frontline/FrontlineApplication.java
@@ -0,0 +1,14 @@
+package zs.frontline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontlineApplication.class, args);
+	}
+
+}
+
---
+++ frontline/src/test/java/zs/frontline/FrontlineApplicationTests.java
@@ -0,0 +1,17 @@
+package zs.frontline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrontlineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
