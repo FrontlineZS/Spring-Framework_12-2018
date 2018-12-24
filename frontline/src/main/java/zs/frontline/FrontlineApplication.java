dc025c5cacad417d95162a496ed988646f694106
radoslaw.kochaniuk <radoslaw.kochaniuk@assecods.pl>
2018-12-24 23:12:28 +0100

    deleted unecessary code

--- frontline/src/main/java/zs/frontline/FrontlineApplication.java
+++ frontline/src/main/java/zs/frontline/FrontlineApplication.java
@@ -14,21 +14,6 @@ public class FrontlineApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(FrontlineApplication.class, args);
-
-		Book book1 = new Book("title1", "isbn1", "publisher1");
-		Book book2 = new Book("title2", "isbn2", "publisher2");
-
-		Set<Book> bookSet = new HashSet<>();
-		bookSet.add(book1);
-		bookSet.add(book2);
-
-		Author author = Author.NameAndSurname("Radosław", "Kochaniuk");
-		Author authorWithBooks = Author.NameSurnameAndBooks("Radosław", "Kochaniuk", bookSet);
-
-		System.out.println("Author id: " + author.getId());
-		System.out.println("Author's name: " + author.getFirstName() + " " + author.getLastName());
-
-		System.out.println("AuthorWithBooks's books: " + authorWithBooks.getBooks());
 	}
 
 }
